5b658222df28c59c356fccd897e03f5e5b5c380a
Polichinel <spm@ifs.ku.dk>
2021-10-21 12:53:47 +0200

    more code added

--- FasterRCNN_fruits.py
+++ FasterRCNN_fruits.py
@@ -29,6 +29,62 @@ from engine import train_one_epoch, evaluate
 import utils
 import transforms as T
 
+# for transformations
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 print(torch.__version__)
 print(torchvision.__version__)
+
+# Correcting the train_one_epoch script - this could go into a util file or something..
+def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
+    model.train()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+
+    lr_scheduler = None
+    # if epoch == 0:
+    #     warmup_factor = 1. / 1000
+    #     warmup_iters = min(1000, len(data_loader) - 1)
+
+        #lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=warmup_factor, total_iters=warmup_iters)
+        #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        # lr_scheduler = lr_scheduler.LinearLR(optimizer, start_factor=warmup_factor, total_iters=warmup_iters)
+
+    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = utils.reduce_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        loss_value = losses_reduced.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            print(loss_dict_reduced)
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+    return metric_logger
+
+files_dir = '/home/projects/ku_00017/data/raw/fruits/train_zip/train'
+test_dir = '/home/projects/ku_00017/data/raw/fruits/test_zip/test'
+
+print(files_dir)
+print(test_dir)
