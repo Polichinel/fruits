79fa740415926f2da4540f0709ee134b0ce721e7
Polichinel <spm@ifs.ku.dk>
2021-10-21 14:19:17 +0200

    added training....

--- FasterRCNN_fruits.py
+++ FasterRCNN_fruits.py
@@ -278,3 +278,37 @@ data_loader = torch.utils.data.DataLoader(
 data_loader_test = torch.utils.data.DataLoader(
     dataset_test, batch_size=10, shuffle=False, num_workers=4,
     collate_fn=utils.collate_fn)
+
+# TRAINING ---------------------------------------------------------------------------------------
+# to train on gpu if selected.
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+num_classes = 4
+
+# get the model using our helper function
+model = get_object_detection_model(num_classes)
+
+# move model to the right device
+model.to(device)
+
+# construct an optimizer
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.005,
+                            momentum=0.9, weight_decay=0.0005)
+
+# and a learning rate scheduler which decreases the learning rate by
+# 10x every 3 epochs
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+# training for 10 epochs
+num_epochs = 10
+# num_epochs = 100
+
+
+for epoch in range(num_epochs):
+    # training for one epoch
+    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+    # update the learning rate
+    lr_scheduler.step()
+    # evaluate on the test dataset
+    evaluate(model, data_loader_test, device=device)
