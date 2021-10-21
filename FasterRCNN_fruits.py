844559a7b03e3f241dda4a62475fa91e34c4407a
Polichinel <spm@ifs.ku.dk>
2021-10-21 11:58:40 +0200

    first commit

---
+++ FasterRCNN_fruits.py
@@ -0,0 +1,34 @@
+# Basic python and ML Libraries
+import os
+import random
+import math
+import numpy as np
+import pandas as pd
+# for ignoring warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# We will be reading images using OpenCV
+import cv2
+
+# xml library for parsing xml files
+from xml.etree import ElementTree as et
+
+# matplotlib for visualization
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# torchvision libraries
+import torch
+import torchvision
+from torchvision import transforms as torchtrans  
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+# these are the helper libraries imported.
+from engine import train_one_epoch, evaluate
+import utils
+import transforms as T
+
+
+print(torch.__version__)
+print(torchvision.__version__)
