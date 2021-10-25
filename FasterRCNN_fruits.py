29ddca508bfff5a68a8a76fc785593726df2ac48
Polichinel <spm@ifs.ku.dk>
2021-10-25 10:38:14 +0200

    moved all function to Utils_fruit

--- FasterRCNN_fruits.py
+++ FasterRCNN_fruits.py
@@ -31,6 +31,12 @@ import transforms as T
 
 # my own utils
 from Utils_fruits import plot_img_bbox
+from Utils_fruits import train_one_epoch # corrected foir linear_lr error
+from Utils_fruits import FruitImagesDataset # the data loader
+from Utils_fruits import get_object_detection_model
+from Utils_fruits import get_transform
+from Utils_fruits import apply_nms
+from Utils_fruits import torch_to_pil
 
 # for transformations
 import albumentations as A
@@ -39,160 +45,9 @@ from albumentations.pytorch import ToTensorV2
 print(torch.__version__)
 print(torchvision.__version__)
 
-# Correcting the train_one_epoch script - this could go into a util file or something..
-def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
-    model.train()
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
-
-    lr_scheduler = None
-    # if epoch == 0:
-    #     warmup_factor = 1. / 1000
-    #     warmup_iters = min(1000, len(data_loader) - 1)
-
-        #lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=warmup_factor, total_iters=warmup_iters)
-        #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-        # lr_scheduler = lr_scheduler.LinearLR(optimizer, start_factor=warmup_factor, total_iters=warmup_iters)
-
-    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
-        images = list(image.to(device) for image in images)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
-        loss_dict = model(images, targets)
-
-        losses = sum(loss for loss in loss_dict.values())
-
-        # reduce losses over all GPUs for logging purposes
-        loss_dict_reduced = utils.reduce_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
-        loss_value = losses_reduced.item()
-
-        if not math.isfinite(loss_value):
-            print("Loss is {}, stopping training".format(loss_value))
-            print(loss_dict_reduced)
-            sys.exit(1)
-
-        optimizer.zero_grad()
-        losses.backward()
-        optimizer.step()
-
-        if lr_scheduler is not None:
-            lr_scheduler.step()
-
-        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
-        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-
-    return metric_logger
-
 # data location ------------------------------------------------------
 files_dir = '/home/projects/ku_00017/data/raw/fruits/train_zip/train'
 test_dir = '/home/projects/ku_00017/data/raw/fruits/test_zip/test'
-
-print(files_dir)
-print(test_dir)
-# --------------------------------------------------------------------
-
-# creating dataloader - could be in a utils files or a totally seperate file..
-class FruitImagesDataset(torch.utils.data.Dataset):
-
-    def __init__(self, files_dir, width, height, transforms=None):
-        self.transforms = transforms
-        self.files_dir = files_dir
-        self.height = height
-        self.width = width
-        
-        # sorting the images for consistency
-        # To get images, the extension of the filename is checked to be jpg
-        self.imgs = [image for image in sorted(os.listdir(files_dir))
-                        if image[-4:]=='.jpg']
-        
-        
-        # classes: 0 index is reserved for background
-        self.classes = ['', 'apple','banana','orange'] # this _ thing does not work on you lap top. Maybe just do ''.
-
-    def __getitem__(self, idx):
-
-        img_name = self.imgs[idx]
-        image_path = os.path.join(self.files_dir, img_name)
-
-        # reading the images and converting them to correct size and color    
-        img = cv2.imread(image_path)
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
-        img_res = cv2.resize(img_rgb, (self.width, self.height), cv2.INTER_AREA)
-        # diving by 255
-        img_res /= 255.0
-        
-        # annotation file
-        annot_filename = img_name[:-4] + '.xml'
-        annot_file_path = os.path.join(self.files_dir, annot_filename)
-        
-        boxes = []
-        labels = []
-        tree = et.parse(annot_file_path)
-        root = tree.getroot()
-        
-        # cv2 image gives size as height x width
-        wt = img.shape[1]
-        ht = img.shape[0]
-        
-        # box coordinates for xml files are extracted and corrected for image size given
-        for member in root.findall('object'):
-            labels.append(self.classes.index(member.find('name').text))
-            
-            # bounding box
-            xmin = int(member.find('bndbox').find('xmin').text)
-            xmax = int(member.find('bndbox').find('xmax').text)
-            
-            ymin = int(member.find('bndbox').find('ymin').text)
-            ymax = int(member.find('bndbox').find('ymax').text)
-            
-            
-            xmin_corr = (xmin/wt)*self.width
-            xmax_corr = (xmax/wt)*self.width
-            ymin_corr = (ymin/ht)*self.height
-            ymax_corr = (ymax/ht)*self.height
-            
-            boxes.append([xmin_corr, ymin_corr, xmax_corr, ymax_corr])
-        
-        # convert boxes into a torch.Tensor
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        
-        # getting the areas of the boxes
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
-        
-        labels = torch.as_tensor(labels, dtype=torch.int64)
-
-
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-        # image_id
-        image_id = torch.tensor([idx])
-        target["image_id"] = image_id
-
-
-        if self.transforms:
-            
-            sample = self.transforms(image = img_res,
-                                     bboxes = target['boxes'],
-                                     labels = labels)
-            
-            img_res = sample['image']
-            target['boxes'] = torch.Tensor(sample['bboxes'])
-            
-            
-            
-        return img_res, target
-
-    def __len__(self):
-        return len(self.imgs)
 
 
 # check dataset
@@ -203,63 +58,10 @@ print('length of dataset = ', len(dataset), '\n')
 img, target = dataset[78]
 print(img.shape, '\n',target)
 
-# # Function to visualize bounding boxes in the image - save the image in the figures folder
-# def plot_img_bbox(img, target, fig_path):
-
-#     # Remove old plot if it exists:
-#     if os.path.exists(fig_path):
-#         os.remove(fig_path)
-
-#     # plot the image and bboxes
-#     # Bounding boxes are defined as follows: x-min y-min width height
-#     fig, a = plt.subplots(1,1)
-#     fig.set_size_inches(5,5)
-#     a.imshow(img)
-#     for box in (target['boxes']):
-#         # specify cpu just in case.
-#         x, y, width, height  = box[0].cpu(), box[1].cpu(), box[2].cpu()-box[0].cpu(), box[3].cpu()-box[1].cpu()
-#         rect = patches.Rectangle((x, y),
-#                                  width, height,
-#                                  linewidth = 2,
-#                                  edgecolor = 'r',
-#                                  facecolor = 'none')
-
-#         # Draw the bounding box on top of the image
-#         a.add_patch(rect)
-#     plt.savefig(fig_path, bbox_inches = "tight")
-    
 # plotting the image with bboxes. Feel free to change the index
 img, target = dataset[25]
 fig_path = '/home/projects/ku_00017/people/simpol/scripts/fruits/figures/plot_1.jpg'
 plot_img_bbox(img, target, fig_path)
-
-
-def get_object_detection_model(num_classes):
-
-    # load a model pre-trained pre-trained on COCO
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    
-    # get number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
-
-    return model
-
-# Send train=True fro training transforms and False for val/test transforms
-def get_transform(train):
-    
-    if train:
-        return A.Compose([
-                            A.HorizontalFlip(0.5),
-                     # ToTensorV2 converts image to pytorch tensor without div by 255
-                            ToTensorV2(p=1.0) 
-                        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
-    else:
-        return A.Compose([
-                            ToTensorV2(p=1.0)
-                        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
-
 
 # prepering the dataset
 # use our dataset and defined transformations
@@ -319,27 +121,9 @@ for epoch in range(num_epochs):
     # evaluate on the test dataset
     evaluate(model, data_loader_test, device=device)
 
-# NEW ----------------------------------------------------------
 
 # Decode predictions
 
-# the function takes the original prediction and the iou threshold.
-
-def apply_nms(orig_prediction, iou_thresh=0.3):
-    
-    # torchvision returns the indices of the bboxes to keep
-    keep = torchvision.ops.nms(orig_prediction['boxes'], orig_prediction['scores'], iou_thresh)
-    
-    final_prediction = orig_prediction
-    final_prediction['boxes'] = final_prediction['boxes'][keep]
-    final_prediction['scores'] = final_prediction['scores'][keep]
-    final_prediction['labels'] = final_prediction['labels'][keep]
-    
-    return final_prediction
-
-# function to convert a torchtensor back to PIL image
-def torch_to_pil(img):
-    return torchtrans.ToPILImage()(img).convert('RGB')
 
 #Testing
 # pick one image from the test set
@@ -367,10 +151,9 @@ print('NMS APPLIED MODEL OUTPUT plottet')
 
 
 # lets try the test set:
-
 test_dataset = FruitImagesDataset(test_dir, 480, 480, transforms= get_transform(train=True))
 # pick one image from the test set
-img, target = test_dataset[9]
+img, target = test_dataset[10]
 # put the model in evaluation mode
 model.eval()
 with torch.no_grad():
--- Utils_fruits.py
+++ Utils_fruits.py
@@ -33,6 +33,147 @@ import transforms as T
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
+# creating dataloader - could be in a utils files or a totally seperate file..
+class FruitImagesDataset(torch.utils.data.Dataset):
+
+    def __init__(self, files_dir, width, height, transforms=None):
+        self.transforms = transforms
+        self.files_dir = files_dir
+        self.height = height
+        self.width = width
+        
+        # sorting the images for consistency
+        # To get images, the extension of the filename is checked to be jpg
+        self.imgs = [image for image in sorted(os.listdir(files_dir))
+                        if image[-4:]=='.jpg']
+        
+        
+        # classes: 0 index is reserved for background
+        self.classes = ['', 'apple','banana','orange'] # this _ thing does not work on you lap top. Maybe just do ''.
+
+    def __getitem__(self, idx):
+
+        img_name = self.imgs[idx]
+        image_path = os.path.join(self.files_dir, img_name)
+
+        # reading the images and converting them to correct size and color    
+        img = cv2.imread(image_path)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
+        img_res = cv2.resize(img_rgb, (self.width, self.height), cv2.INTER_AREA)
+        # diving by 255
+        img_res /= 255.0
+        
+        # annotation file
+        annot_filename = img_name[:-4] + '.xml'
+        annot_file_path = os.path.join(self.files_dir, annot_filename)
+        
+        boxes = []
+        labels = []
+        tree = et.parse(annot_file_path)
+        root = tree.getroot()
+        
+        # cv2 image gives size as height x width
+        wt = img.shape[1]
+        ht = img.shape[0]
+        
+        # box coordinates for xml files are extracted and corrected for image size given
+        for member in root.findall('object'):
+            labels.append(self.classes.index(member.find('name').text))
+            
+            # bounding box
+            xmin = int(member.find('bndbox').find('xmin').text)
+            xmax = int(member.find('bndbox').find('xmax').text)
+            
+            ymin = int(member.find('bndbox').find('ymin').text)
+            ymax = int(member.find('bndbox').find('ymax').text)
+            
+            
+            xmin_corr = (xmin/wt)*self.width
+            xmax_corr = (xmax/wt)*self.width
+            ymin_corr = (ymin/ht)*self.height
+            ymax_corr = (ymax/ht)*self.height
+            
+            boxes.append([xmin_corr, ymin_corr, xmax_corr, ymax_corr])
+        
+        # convert boxes into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        
+        # getting the areas of the boxes
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+        
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+        # image_id
+        image_id = torch.tensor([idx])
+        target["image_id"] = image_id
+
+
+        if self.transforms:
+            
+            sample = self.transforms(image = img_res,
+                                     bboxes = target['boxes'],
+                                     labels = labels)
+            
+            img_res = sample['image']
+            target['boxes'] = torch.Tensor(sample['bboxes'])
+            
+            
+            
+        return img_res, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+# Correcting the train_one_epoch script - this could go into a util file or something..
+def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
+    model.train()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+
+    lr_scheduler = None
+
+    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = utils.reduce_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        loss_value = losses_reduced.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            print(loss_dict_reduced)
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+    return metric_logger
+
 # Function to visualize bounding boxes in the image - save the image in the figures folder
 def plot_img_bbox(img, target, fig_path):
 
@@ -57,3 +198,48 @@ def plot_img_bbox(img, target, fig_path):
         # Draw the bounding box on top of the image
         a.add_patch(rect)
     plt.savefig(fig_path, bbox_inches = "tight")
+
+
+def get_object_detection_model(num_classes):
+
+    # load a model pre-trained pre-trained on COCO
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
+
+    return model
+
+# Send train=True fro training transforms and False for val/test transforms
+def get_transform(train):
+    
+    if train:
+        return A.Compose([
+                            A.HorizontalFlip(0.5),
+                     # ToTensorV2 converts image to pytorch tensor without div by 255
+                            ToTensorV2(p=1.0) 
+                        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+    else:
+        return A.Compose([
+                            ToTensorV2(p=1.0)
+                        ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
+
+# the function takes the original prediction and the iou threshold.
+def apply_nms(orig_prediction, iou_thresh=0.3):
+    
+    # torchvision returns the indices of the bboxes to keep
+    keep = torchvision.ops.nms(orig_prediction['boxes'], orig_prediction['scores'], iou_thresh)
+    
+    final_prediction = orig_prediction
+    final_prediction['boxes'] = final_prediction['boxes'][keep]
+    final_prediction['scores'] = final_prediction['scores'][keep]
+    final_prediction['labels'] = final_prediction['labels'][keep]
+    
+    return final_prediction
+
+# function to convert a torchtensor back to PIL image
+def torch_to_pil(img):
+    return torchtrans.ToPILImage()(img).convert('RGB')
